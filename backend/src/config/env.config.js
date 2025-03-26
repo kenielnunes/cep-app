4dd55d0466738d070f2735de7994063ceb22c868
Keniel Nunes <kenielalves3@gmail.com>
2025-03-26 14:36:56 -0300

    fix: validate envs

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -10,6 +10,11 @@ const validateEnv = (env, envName) => {
 };
 
 export const env = {
+  POSTGRES_USER: validateEnv(process.env.POSTGRES_USER, 'POSTGRES_USER'),
+  POSTGRES_PASSWORD: validateEnv(process.env.POSTGRES_PASSWORD, 'POSTGRES_PASSWORD'),
+  POSTGRES_DB: validateEnv(process.env.POSTGRES_DB, 'POSTGRES_DB'),
+  POSTGRES_HOST: validateEnv(process.env.POSTGRES_HOST, 'POSTGRES_HOST'),
+  POSTGRES_PORT: validateEnv(process.env.POSTGRES_PORT, 'POSTGRES_PORT'),
   CEP_EXTERNAL_URL_API: validateEnv(process.env.CEP_EXTERNAL_URL_API, 'CEP_EXTERNAL_URL_API'),
   JWT_SECRET: validateEnv(process.env.JWT_SECRET, 'JWT_SECRET'),
   HASH_CRYPTO_SECRET: validateEnv(process.env.HASH_CRYPTO_SECRET, 'HASH_CRYPTO_SECRET')
